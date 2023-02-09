7b5864ff9df0a432e3c88ab40d0a0f07f659b83f
bs585@njit.edu <bs585@njit.edu>
2023-02-08 22:24:14 -0500

    saving

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -3,38 +3,59 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('login.php.inc');
-
-function doLogin($username,$password)
-{
-    // lookup username in databas
-    // check password
-    $login = new loginDB();
-    return $login->validateLogin($username,$password);
-    //return false if not valid
-}
-
-function requestProcessor($request)
-{
-  echo "received request".PHP_EOL;
-  var_dump($request);
-  if(!isset($request['type']))
-  {
-    return "ERROR: unsupported message type";
-  }
-  switch ($request['type'])
-  {
-    case "login":
-      return doLogin($request['username'],$request['password']);
-    case "validate_session":
-      return doValidate($request['sessionId']);
-  }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
-}
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
+echo "testRabbitMQServer BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
 exit();
-?>
 
+function requestProcessor($request)
+{
+    echo "received request".PHP_EOL;
+    echo "DoLogin Function".PHP_EOL;
+    registerUser($request);
+  var_dump($request);
+}
+
+function registerUser($request)
+{
+    echo "register user called".PHP_EOL;
+    $mydb = new mysqli('127.0.0.1','testuser','12345','testdb');
+
+
+    if ($mydb->errno != 0)
+    {
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+        exit(0);
+     }
+    echo "are we here".PHP_EOL;
+     if(!isset($request['type']))
+     {
+         return "ERROR: unsupported message type";
+     }
+
+     switch ($request['type']) 
+     {
+          case "validate":
+                $username = $request['username'];
+                $query = "SELECT * FROM users WHERE username = '$username';";
+                $response = $mydb->query($query);
+        echo "Here as well".PHP_EOL;
+                if (mysqli_num_rows($response) > 0) //already present
+                {
+                    $password = $request['password'];
+                    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password';";
+                    if (mysqli_num_rows($response) > 0)
+            {
+            echo "We correctly worked".PHP_EOL;
+                        return array("returnCode" => '0', 'message'=>"Valid Login");            
+                    }
+                }
+
+                return array("returnCode" => '0', 'message'=>"Invalid Login");
+     }
+}
+
+?>
